07add3ca896f3c2bab8626d41bc36818aa5083d4
Kuczer <goabeach604@gmail.com>
2021-10-04 18:12:51 +0200

    added more commands, page objects and tests

--- cypress/support/pageObjects/ShoesCategoryDetails_PO.js
+++ cypress/support/pageObjects/ShoesCategoryDetails_PO.js
@@ -1,7 +1,15 @@
+import { productDetailsSelectors } from "../selectors/selectors";
 
 class ShoesCategoryDetails_PO {
-  OpenShoesDetails(name) {
-    cy.openProduct(name); 
+  openShoesDetails(name, url) {
+    cy.openProduct(name);
+    cy.checkURL(url);
+  }
+  chooseSize(option) {
+    cy.chooseOptionFromCheckbox(option);
+  }
+  changeTheQty(value) {
+    cy.clearInputAndType(productDetailsSelectors.qty, value);
   }
 }
 export default ShoesCategoryDetails_PO;
